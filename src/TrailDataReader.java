ee4d2358361ef9f520b717b82f4638e7491fbe4b
yudithmendoza <159387580+yudithmendoza@users.noreply.github.com>
2024-03-18 17:58:47 -0600

    Create TrailDataReader.java

---
+++ src/TrailDataReader.java
@@ -0,0 +1,12 @@
+package a07;
+
+/**
+ * Read data from CSV file. Test Client
+ * @author Shala and Yudith
+ */
+public class TrailDataReader {
+
+	public static void main(String[] args) {
+		//TODO
+	}
+}
