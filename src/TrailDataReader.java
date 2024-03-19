322e064e4a9ec43a984259a3b7ba76837fdbc082
shala <104413693+itsshalag@users.noreply.github.com>
2024-03-19 19:30:07 -0400

    Update TrailDataReader.java
    Add challenge 4

--- src/TrailDataReader.java
+++ src/TrailDataReader.java
@@ -73,19 +73,18 @@ public class TrailDataReader {
 	public static void main(String[] args) {
 		String filePath = "Resources/TrailsAndPathways.csv"; 
 		
+		// read from the CSV file
 		List<TrailPathData> dataList = TrailDataReader.readData(filePath);
 		
 		// symbol table to store trail data
 		ST<Integer, TrailPathData> symbolTable = new ST<>();  
 		
+		// add existing trails to the symbol table
 		for (TrailPathData data : dataList) {
 			symbolTable.put(data.getObjectID(), data); 
 		}
-		
-//		System.out.println("Symbol Table:"); 
 
-		// Challenge 1: used keys() and get() methods from ST table to look for
-		// trails in Salt Lake County; 
+		// Challenge 1: used keys() to look for trails in Salt Lake County; 
 		String countyToSearch = "Salt Lake"; 
 		int count =0;
 		
@@ -93,23 +92,35 @@ public class TrailDataReader {
 			TrailPathData trailData = symbolTable.get(trailID); 
 			if (trailData.getCounty().equalsIgnoreCase(countyToSearch)) {
 				count++; 
-//				System.out.println(trailData.toString()); 
 			}
 		}
 		System.out.println("Number of trails in " + countyToSearch + ": " + count); 
 		
 
-		// Challenge 2: Find the trail with a shape length closets to but not exceeding 100 using floor(). 
+		// Challenge 2: Find the trail with a shape length closest to but not exceeding 100 using floor(). 
 		double maxLength = 100.00;
 		Integer floorTrailID = symbolTable.floor((int) maxLength);
 		TrailPathData floorTrail = symbolTable.get(floorTrailID); 
-		System.out.println("\nName of county with a trail of shape length closest to but not exceeding 100: " + floorTrail.getCounty()); 
-		
+		System.out.println("\nName of county with a trail of shape length closest to but not exceeding 100: " + floorTrail.getCounty()); 		
 		
 		// Challenge 3: Use ceiling() to find the smallest key greater than or equal to 7,000. 
 		double minLength = 7000.00;
 		Integer ceilingTrailID = symbolTable.ceiling((int) minLength); 
 		TrailPathData ceilingTrail = symbolTable.get(ceilingTrailID); 
-		System.out.println("\nName of county with a trail of shape length closes but not less than 7,000: " + ceilingTrail.getCounty());
-	}	
+		System.out.println("\nName of county with a trail of shape length closest but not less than 7,000: " + ceilingTrail.getCounty()); 
+		
+		//Challenge 4: Add a new trail to the symbol table and print it.  
+		TrailPathData newTrail = new TrailPathData(41728, "New Trail", "4- Road-concurrent", "Salt Lake", new Date(124, 2, 19), new Date(124, 2, 19), 100.0); 
+		symbolTable.put(newTrail.getObjectID(), newTrail);
+		System.out.println("\nNew trail added: \n" + newTrail); 
+		System.out.println(); 		
+		
+		//Displays updated list
+//		System.out.println("\nUpdate list of trails: ");
+//		for (Integer trailID : symbolTable.keys()) {
+//			TrailPathData trailData = symbolTable.get(trailID); 
+//			System.out.println(trailData);
+//		}
+	
+	}
 }
